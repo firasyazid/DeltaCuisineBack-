bade8f8c8c289babfe6ae0c798179d32e514828c
firasyazid <98762038+firasyazid@users.noreply.github.com>
2023-11-23 06:21:34 +0100

    commot

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -1,1 +1,25 @@
+const expressJwt = require('express-jwt');
  
+function authJwt() {
+    const secret = process.env.secret;
+ 
+    return expressJwt({
+      secret,
+      algorithms: ['HS256'],
+      isRevoked: isRevoked
+   }).unless({
+      path: [
+         { url: /(.*)/ }
+      ]
+   });
+  }   
+
+    
+    async function isRevoked(req, payload, done) {
+      if(!payload.isAdmin) {
+          done(null, true)
+      }
+   
+      done();
+   }
+    module.exports= authJwt;
